e82af20b188e347ff71c6469bc41455a7de4b3e5
joemelt101 <joewebsites@outlook.com>
2016-02-02 02:16:14 -0600

    First milestone completed.

--- OSS16_Project1/src/process_scheduling.c
+++ OSS16_Project1/src/process_scheduling.c
@@ -14,6 +14,49 @@ void virtual_cpu(ProcessControlBlock_t* process_control_block) {
 }
 
 bool first_come_first_serve(dyn_array_t* ready_queue, ScheduleResult_t* result) {
+    if (! ready_queue || ! result)
+    {
+        return false;
+    }
+    
+    //setup queue
+    ProcessControlBlock_t *pcb;
+    
+    //Prep statistics calculations
+    int numProcesses = dyn_array_size(ready_queue);
+    result->average_latency_time = 0.0f;
+    result->average_wall_clock_time = 0.0f;
+    result->total_run_time = 0;
+    
+    while (dyn_array_empty(ready_queue) == false)
+    {
+        result->average_latency_time += result->total_run_time;
+        
+        //crank through another process
+        pcb = (ProcessControlBlock_t *)dyn_array_back(ready_queue);
+        
+        if (pcb != NULL)
+        {
+            //process the block
+            while (pcb->remaining_burst_time)
+            {
+                //cycle through
+                virtual_cpu(pcb);
+                result->total_run_time++;
+            }
+            
+            //cleanup
+            dyn_array_pop_back(ready_queue);
+        }
+        
+        result->average_wall_clock_time += result->total_run_time;
+    }
+    
+    //finished running all processes
+    //divide out to find averages
+    result->average_latency_time /= numProcesses;
+    result->average_wall_clock_time /= numProcesses;
+    
 	return true;
 }
 
